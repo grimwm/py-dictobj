2d7ffee74bbdc9bb54e85c22b4604b747df4db2e
William Grim <william@grimapps.com>
2012-12-31 00:17:08 -0500

    Set up the project for a release on PyPi.

--- setup.py
+++ setup.py
@@ -2,9 +2,19 @@ from setuptools import setup
 
 setup(
   name='dictobj',
-  version='0.1',
+  version='0.1.1',
   author='William Grim',
   author_email='william@grimapps.com',
+  url='https://github.com/grimwm/py-dictobj',
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+  description='',
+  long_description='',
   py_modules=['dictobj'],
   test_suite='dictobj_test'
   )
