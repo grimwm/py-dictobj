3457bf5a17e7813b0ea5a67f8dd0e2b551df53f4
William Grim <william@grimapps.com>
2012-12-30 20:05:08 -0500

    Add a setup.py module so devs can build and install this package.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='dictobj',
+  version='0.1',
+  author='William Grim',
+  author_email='william@grimapps.com',
+  py_modules=['dictobj'],
+  test_suite='dictobj_test'
+  )
