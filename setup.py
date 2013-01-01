1b98defd5a2bd7d18971d7a10d2527ea55681e6b
William Grim <william@grimapps.com>
2013-01-01 17:54:26 -0500

    Upgrade to 0.2.1 and add a description.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='dictobj',
-  version='0.2',
+  version='0.2.1',
   author='William Grim',
   author_email='william@grimapps.com',
   url='https://github.com/grimwm/py-dictobj',
@@ -13,8 +13,7 @@ setup(
     'Programming Language :: Python',
     'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-  description='',
-  long_description='',
+  description='A set of Python dictionary objects where keys can be accessed as instnace attributes.',
   py_modules=['dictobj'],
   test_suite='dictobj_test'
   )
