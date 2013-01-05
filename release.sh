2998f02c0e82568b005425b1800f5ba30dbb7ca9
William Grim <william@grimapps.com>
2013-01-05 00:41:54 -0500

    Slight expansion of the release script to generate PyPI documentation.

--- release.sh
+++ release.sh
@@ -3,9 +3,11 @@
 # Run build
 pandoc README.md -w rst -o README.txt
 python setup.py sdist upload
+markdown README.md > index.html
+zip pypi.zip index.html
 
 # Do cleanup
-rm -f README.txt
+rm -f README.txt index.html
 rm -rf dist
 rm -rf dictobj.egg-info
 rm -rf __pycache__
